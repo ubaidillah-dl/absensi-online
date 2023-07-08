4f95dd33b75675051bb0bd09bf0e3ea92eef387b
ubaidillah-dl <ubayd2947@gmail.com>
2023-07-08 17:00:48 +0700

    tes

--- absen.php
+++ absen.php
@@ -144,7 +144,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Absen</title>
+    <title>Absensi</title>
 
     <link rel="icon" type="image/x-icon" href="assets/favicon.png">
     <link href="css/bootstrap.min.css" rel="stylesheet">
